fb28091331592e6fa6ba3f98f249bffdae4ab4ff
JulioVillalobos03 <julioalbertoocampovillalobos2@gmail.com>
2026-01-21 09:13:24 -0600

    Version 1.0.0 - test-env

--- src/config/env.js
+++ src/config/env.js
@@ -2,15 +2,15 @@
 export const env = {
   port: process.env.PORT || 3000,
   db: {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    name: process.env.DB_NAME,
+    host: process.env.DB_HOST || "srv1624.hstgr.io",
+    port: process.env.DB_PORT || 3306,
+    user: process.env.DB_USER || "u608641600_lumera_core",
+    password: process.env.DB_PASSWORD || "LumeraCoreDB26@",
+    name: process.env.DB_NAME || "u608641600_LUMERA_Core",
   },
 
   jwt: {
-    secret: process.env.JWT_SECRET,
+    secret: process.env.JWT_SECRET || lumera_super_secret_key,
     expiresIn: process.env.JWT_EXPIRES_IN || "1d",
   },
 };
