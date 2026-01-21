8b216926a60ad4f168c1e12e3a6c8cc00b7ee3c9
JulioVillalobos03 <julioalbertoocampovillalobos2@gmail.com>
2026-01-21 09:13:24 -0600

    Version 1.0.0

--- src/server.js
+++ src/server.js
@@ -1,12 +1,18 @@
 import "dotenv/config";
 import app from './app.js';
 import { env } from './config/env.js';
+import cors from 'cors';
 
 app.listen(env.port, () => {
      console.log("PORT:", env.port);
     console.log("JWT_SECRET:", env.jwt.secret ? "OK" : "MISSING");
     console.log(`Server running on port ${env.port}`);
 });
+
+app.use(cors({
+  origin: 'https://greenyellow-pony-308023.hostingersite.com/', // Permite que tu frontend local se conecte
+  credentials: true
+}));
 
 // en routes o directamente en server.js
 app.get("/api/test-db", async (req, res) => {
